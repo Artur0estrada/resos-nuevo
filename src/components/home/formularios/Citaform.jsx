8b4dbd6feb553850aa137c6a535fb8980274801f
Artur0estrada <luis_estradaR89@hotmail.com>
2022-11-21 18:09:39 -0700

    Salmos 23

--- src/components/home/formularios/Citaform.jsx
+++ src/components/home/formularios/Citaform.jsx
@@ -1,13 +1,14 @@
-import {useForm} from "react-hook-form";
+import {useForm } from "react-hook-form";
 
 const Citaform = () =>{
 
-const { register, handleSubmit } = useForm();
+const { register, handleSubmit, watch } = useForm();
 
 const onSubmit =(data) =>{
     console.log(data)
 }
 
+const Instrumentos = watch('Instrumento');
 
     return <div>
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -24,18 +25,49 @@ const onSubmit =(data) =>{
 
             </div>
             <div>
-                    <select class = "textos" placeholder={'Instrumento'} {...register('Instrumento')}>
-                        <option value={"Guitarra Acústica/Electroacustica"}>Guitarra acústica o electroacustica</option>
+                <select class={'textos'} name={'Instrumentos'} {...register('Instrumento')}>
+                    <option {...register('Bajos')} value={"Bajo Eléctrico"}>Bajo Eléctrico</option>
+                    <option value={"Guitarra Electrica"}>Guitarra Electrica</option>
+                    <option value={"Guitarra Acustica/Electroacustica"}>Guitarra Acustica/Electroacustica</option>
+                    <option value={"Instrumento Regional"}>Instrumento Regional</option>
+                </select>
+            </div>
+            {Instrumentos === 'Bajo Eléctrico' && (
+                <div>
+                    <select class={'textos'} >
                         <option value={"Bajo Eléctrico 4 cuerdas"}>Bajo de 4 cuerdas</option>
                         <option value={"Bajo Eléctrico 5 cuerdas"}>Bajo de 5 cuerdas</option>
                         <option value={"Bajo Eléctrico 6 cuerdas"}>Bajo de 6 cuerdas</option>
-                        <option value={"Guitarra electrica"}>Guitarra Electrica</option>
-                        <option value={"Guitarra electrica Floyd Rose"}>Guitarra Electrica Floyd Rose</option>
+                    </select>
+                </div>
+            )}
+            {Instrumentos === 'Guitarra Electrica' && (
+                <div>
+                    <select class={'textos'} >
+                        <option value={"Puente Fijo"}>Puente Fijo</option>
+                        <option value={"Puente Tremono"}>Puente Tremono</option>
+                        <option value={"Floyd Rose"}>Floyd Rose</option>
+                    </select>
+                </div>
+            )}
+            {Instrumentos === 'Guitarra Acustica/Electroacustica' && (
+                <div>
+                    <select class={'textos'} >
+                        <option value={"Acustica"}>Acustica</option>
+                        <option value={"Electroacustica"}>Electroacustica</option>
+                        <option value={"Clasica"}>Clasica</option>
+                    </select>
+                </div>
+            )}
+            {Instrumentos === 'Instrumento Regional' && (
+                <div>
+                    <select class={'textos'} >
                         <option value={"Guitarra de 12 cuerdas"}>Guitarra de 12 cuerdas</option>
                         <option value={"Bajo Quinto"}>Bajo Quinto</option>
                         <option value={"Bajo sexto"}>Bajo Sexto</option>
                     </select>
-            </div>
+                </div>
+            )}
             <div>
                 <select class = "textos" placeholder={'Servicios'} {...register('Servicio')}>
                     <option value={"Calibración"}>Calibración</option>
